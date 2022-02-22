e76e63d925a96a5b6a2c4959daabf0527440ee8a
emrahisik <emrahisk@gmail.com>
2022-02-21 22:31:56 -0500

    console logs removed

--- middlewares/cleanCache.js
+++ middlewares/cleanCache.js
@@ -1,10 +1,6 @@
 const { clearCache } = require('../services/cache');
 
 module.exports =  (req,res,next) => {
-    console.log("before next")
     next();
-    console.log("after next")
-    console.log("before clear")
     clearCache(req.user.id)
-    console.log("after clear")
 }
